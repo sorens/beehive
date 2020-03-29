8801f39c953e5da922fb4a9606fe68c1dc2b95aa
Steve Orens <steve@orens.com>
2020-03-29 15:21:18 -0700

    Restructure program to allow for different commands

--- main.py
+++ main.py
@@ -122,8 +122,76 @@ def count_unique_letters_in_word(word):
 # No cussing either, sorry.
 # Letters can be used more than once.
 def beehive(dictionary, letters, center_letter, debug, path, level, is_stdout):
-    answers = []
-    return answers
+    output_log(output, "{:<32s} {:>6s}".format("letters:", letters.upper()))
+    output_log(output, "{:<32s} {:>6s}".format("center letter:", center.upper()))
+
+    center_matched = {}
+    for word in dictionary:
+        if center in word:
+            center_matched[word] = dictionary[word]
+
+    output_log(output, "{:<32s} {:>6s}".format("words checked:", str(len(dictionary.keys()))))
+    output_log(output, "{:<32s} {:>6s}".format("words with center letter:", str(len(center_matched.keys()))))
+
+    matched = {}
+    for word in center_matched:
+        found = True
+        for letter in word:
+            if found:
+                if args.debug:
+                    debug_log(output, "checking letter: '" + letter + "' from word: '" + word + "'")
+                if letter not in all_letters:
+                    found = False
+                    break
+        
+        if found:
+            if args.debug:
+                debug_log(output, "" + word + " ADDED")
+            matched[word] = center_matched[word]
+            found = False
+        else:
+            if args.debug:
+                debug_log(output, "" + word + " SKIPPED")
+
+    output_log(output, "{:<32s} {:>6s}".format("words matched with all letters:", str(len(matched))))
+
+    # remove words that are not in more than 'level' dictionaries
+    # if level is 0, inclue all anwers
+    likely_words = {}
+    total_score = 0
+    for word in matched:
+        if level == 0 or matched[word][0] >= level:
+            likely_words[word] = matched[word]
+            total_score += matched[word][1]
+
+    output_log(output, "{:<32s} {:>6s}".format("likely good word:", str(len(likely_words))))
+    output_log(output, "{:<32s} {:>6s}".format("total score:", str(total_score)))
+
+    h = {}
+    for letter in all_letters:
+        h[letter] = []
+
+    sorted_answers = []
+    # sort by length
+    for word in sorted(likely_words, key=lambda word: len(word), reverse=True):
+        sorted_answers.append(word)
+        h[word[0]].append(word)
+
+    for key in h.keys():
+        output_log(output, "{:21s} \"{:<1s}\"        {:>4s} {:>4s}".format("words that begin with", key.upper(), "score", "level"))
+        output_log(output, "{:<32s} {:>4s} {:>4s}".format("-------------------------", "-----", "-----"))
+        for word in h[key]:
+            output_log(output, "{:3}{:<29s} {:>4d} {:>4d}".format("=> ", word, dictionary[word][1], dictionary[word][0]))
+
+def pangrams(dictionary):
+    words = {}
+    for word in dictionary:
+        if count_unique_letters_in_word(word) == 7:
+            words[word] = word
+
+    output_log(output, "{:<32s} {:>15d}".format("number of pangram words:", len(words)))
+    for word in words:
+        output_log(output, "{:<32s} {:>15s}".format("pangram word:", word))
 
 parser = argparse.ArgumentParser(description='beehive puzzle solver')
 parser.add_argument("--letters", type=str, help="non-center letters from beehive board", required=False, default="")
@@ -133,14 +201,9 @@ parser.add_argument("--path", type=str, help="location to output answers", requi
 parser.add_argument("--level", type=int, help="number of dictionary a matched word should appear in", required=False, default=0)
 parser.add_argument("--stdout", help="", action='store_true', default=False, required=False)
 parser.add_argument("--pangrams", help="List all pangram words", action='store_true', default=False, required=False)
+parser.add_argument("--command", help="which command to run (e.g. play, pangrams)", required=True)
 args = parser.parse_args()
 is_stdout = args.stdout
-letters = ""
-center = ""
-if args.letters != "":
-    letters = args.letters.rstrip().lower()
-if args.center != "":
-    center = args.center.rstrip().lower()
 
 output_path = ""
 if is_stdout == False:
@@ -148,95 +211,40 @@ if is_stdout == False:
         output_path = args.path.rstrip().lower()
     else:
         is_stdout = True
-all_letters = letters + center
-level = args.level
 
 if is_stdout == False:
     output_log(sys.stdout, "behive running...")
 
-output_path = os.path.join(output_path, all_letters + "." + "txt")    
-
-if is_stdout:
-    output = sys.stdout
-else:
-    output = open(output_path, 'w')
-
-dictionary = load_dictionaries("word_files", all_letters)
-
-# handle --pangrams
-if args.pangrams:
-    words = {}
-    for word in dictionary:
-        if count_unique_letters_in_word(word) == 7:
-            words[word] = word
-
-    output_log(output, "{:<32s} {:>15d}".format("number of pangram words:", len(words)))
-    for word in words:
-        output_log(output, "{:<32s} {:>15s}".format("pangram word:", word))
-    exit()
-
-beehive(dictionary, letters, center, args.debug, output_path, level, is_stdout)
-
-output_log(output, "{:<32s} {:>6s}".format("letters:", letters.upper()))
-output_log(output, "{:<32s} {:>6s}".format("center letter:", center.upper()))
-
-center_matched = {}
-for word in dictionary:
-    if center in word:
-        center_matched[word] = dictionary[word]
-
-output_log(output, "{:<32s} {:>6s}".format("words checked:", str(len(dictionary.keys()))))
-output_log(output, "{:<32s} {:>6s}".format("words with center letter:", str(len(center_matched.keys()))))
-
-matched = {}
-for word in center_matched:
-    found = True
-    for letter in word:
-        if found:
-            if args.debug:
-                debug_log(output, "checking letter: '" + letter + "' from word: '" + word + "'")
-            if letter not in all_letters:
-                found = False
-                break
-    
-    if found:
-        if args.debug:
-            debug_log(output, "" + word + " ADDED")
-        matched[word] = center_matched[word]
-        found = False
+all_letters = ""
+# check which command we're running
+if args.command == "play":
+    letters = ""
+    center = ""
+    if args.letters != "":
+        letters = args.letters.rstrip().lower()
+    if args.center != "":
+        center = args.center.rstrip().lower()
+    all_letters = letters + center
+    level = args.level
+    output_path = os.path.join(output_path, all_letters + "." + "txt")    
+    if is_stdout:
+        output = sys.stdout
     else:
-        if args.debug:
-            debug_log(output, "" + word + " SKIPPED")
+        output = open(output_path, 'w')
 
-output_log(output, "{:<32s} {:>6s}".format("words matched with all letters:", str(len(matched))))
+    dictionary = load_dictionaries("word_files", all_letters)
+    beehive(dictionary, letters, center, args.debug, output_path, level, is_stdout)
 
-# remove words that are not in more than 'level' dictionaries
-# if level is 0, inclue all anwers
-likely_words = {}
-total_score = 0
-for word in matched:
-    if level == 0 or matched[word][0] >= level:
-        likely_words[word] = matched[word]
-        total_score += matched[word][1]
+elif args.command == "pangrams":
+    output = sys.stdout
+    dictionary = load_dictionaries("word_files", all_letters)
+    pangrams(dictionary)
+else:
+    print("failure")
 
-output_log(output, "{:<32s} {:>6s}".format("likely good word:", str(len(likely_words))))
-output_log(output, "{:<32s} {:>6s}".format("total score:", str(total_score)))
+exit()
 
-h = {}
-for letter in all_letters:
-    h[letter] = []
 
-sorted_answers = []
-# sort by length
-for word in sorted(likely_words, key=lambda word: len(word), reverse=True):
-    sorted_answers.append(word)
-    h[word[0]].append(word)
-
-for key in h.keys():
-    output_log(output, "{:21s} \"{:<1s}\"        {:>4s} {:>4s}".format("words that begin with", key.upper(), "score", "level"))
-    output_log(output, "{:<32s} {:>4s} {:>4s}".format("-------------------------", "-----", "-----"))
-    for word in h[key]:
-        output_log(output, "{:3}{:<29s} {:>4d} {:>4d}".format("=> ", word, dictionary[word][1], dictionary[word][0]))
 
 output.close() 
 
