58cc5720da87d087f89de32ae664639b3e74be02
Steve Orens <steve@orens.com>
2020-04-14 02:44:21 -0700

    Unit test initial commit

---
+++ test_beehive_unittest.py
@@ -0,0 +1,168 @@
+# import unittest
+
+# class TestSum(unittest.TestCase):
+
+#     def test_sum(self):
+#         self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+
+#     def test_sum_tuple(self):
+#         self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
+
+# if __name__ == '__main__':
+#     unittest.main()
+
+# ./beehive --letters NHUTCM --center O
+
+# Coconut
+# Cocoon
+# Coho
+# Common
+# Conch
+# Concoct
+# Coot
+# Cotton
+# Cottonmouth
+# Couch
+# Count
+# Couth
+# Cutout
+# Honcho
+# Hooch
+# Hoot
+# Mono
+# Month
+# Mooch
+# Moon
+# Moot
+# Moth
+# Motto
+# Mount
+# Mouth
+# Mutton
+# Noon
+# Notch
+# Noun
+# Onto
+# Ouch
+# Thou
+# Toon
+# Toot
+# Tooth
+# Touch
+# Tout
+# Uncommon
+# Uncouth
+# Unto
+
+# ➜ ./beehive --letters DHYPIT --center O
+# Ditto
+# Dodo
+# Dopy
+# Doth
+# Dotty
+# Hippo
+# Hood
+# Hoodoo
+# Hoop
+# Hoot
+# Hoppy
+# Hotpot
+# Hyoid
+# Hypo
+# Idiot
+# Oddity
+# Opioid
+# Photo
+# Pooh
+# Poop
+# Poppy
+# Potty
+# Tiptop
+# Toddy
+# Toot
+# Tooth
+# Toothy
+# Typhoid
+# Typo
+
+# ./beehive --command play --letters AHLTRY --center i --level 3
+# Hilarity
+# Airily
+# Airy
+# Alit
+# Aria
+# Aril
+# Atilt
+# Atria
+# Atrial
+# Hail
+# Hair
+# Hairy
+# Hiatal
+# Hill
+# Hilly
+# Hilt
+# Hiya
+# Illy
+# Itty
+# Lair
+# Laity
+# Lariat
+# Liar
+# Lilt
+# Lily
+# Lira
+# Rail
+# Raita
+# Rarity
+# Rattail
+# Rial
+# Rill
+# Riyal
+# Tail
+# Tali
+# Thirty
+# Thrill
+# Tiara
+# Till
+# Tilt
+# Trail
+# Trait
+# Trial
+# Trill
+
+# ./beehive --command play --letters kulcnbe --center l
+# Bell
+# Belle
+# Blue
+# Bluebell
+# Bubble
+# Buckle
+# Bulb
+# Bulk
+# Bull
+# Celeb
+# Cell
+# Cellule
+# Club
+# Cluck
+# Clue
+# Clunk
+# Cull
+# Keel
+# Kennel
+# Kneel
+# Knell
+# Knuckle
+# Leek
+# Lube
+# Luck
+# Lull
+# Lulu
+# Lune
+# Lunk
+# Nubble
+# Null
+# Ukulele
+# Unbuckle
+# Uncle
